aad5807527030c5c9ea1bca68df35e49e5df9f4f
Andrew Godwin <andrew@aeracode.org>
2020-06-15 21:45:12 -0600

    Add actual weather info!

--- server/server.py
+++ server/server.py
@@ -1,16 +1,22 @@
 import datetime
 import requests
-import pytz
+import os
 from io import BytesIO
 
+import pytz
 from PIL import Image, ImageDraw, ImageFont
 from flask import Flask, send_file, request
 
+from weather import WeatherClient
+
 IMAGE_SIZE = (600, 448)
-BLACK = 0
+WHITE = 0
+BLACK = 2
 RED = 1
+PALETTE = [255, 255, 255, 200, 0, 0, 0, 0, 0]
 app = Flask(__name__)
 fonts = {}
+icons = {}
 
 
 @app.route("/")
@@ -19,49 +25,14 @@ def index():
     api_key = request.args.get("api_key")
     if not api_key:
         return '{"error": "no_api_key"}'
-    # Get lat and long
-    lat = request.args.get("latitude", "39.75")
-    long = request.args.get("longitude", "-104.90")
-    timezone = request.args.get("timezone", "America/Denver")
-    # Fetch weather
-    weather_data = requests.get(
-        f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={long}&exclude=minutely&units=metric&appid={api_key}"
-    ).json()
-    temp_current = round(float(weather_data["current"]["temp"]))
-    temp_min = round(float(weather_data["daily"][0]["temp"]["min"]))
-    temp_max = round(float(weather_data["daily"][0]["temp"]["max"]))
-    # Work out time
-    now = datetime.datetime.now(pytz.timezone(timezone))
-    # Create image
-    image = Image.new("P", IMAGE_SIZE, 2)
-    image.putpalette([0, 0, 0, 200, 0, 0, 255, 255, 255] + (252 * [0, 0, 0]))
-    # Draw on date
-    draw = ImageDraw.ImageDraw(image)
-    left = 20
-    top = 10
-    draw_text(draw, left, top, now.strftime("%A"), BLACK, "light", 60)
-    top += 65
-    day_size = draw_text(
-        draw, left, top, now.strftime("%d").lstrip("0"), RED, "regular", 30
+    # Render
+    composer = ImageComposer(
+        api_key,
+        lat=request.args.get("latitude", "39.75"),
+        long=request.args.get("longitude", "-104.90"),
+        timezone=request.args.get("timezone", "America/Denver"),
     )
-    left += day_size[0] + 2
-    th = {
-        "01": "st",
-        "02": "nd",
-        "03": "rd",
-        "21": "st",
-        "22": "nd",
-        "23": "rd",
-        "31": "st",
-    }.get(now.strftime("%d"), "th")
-    th_size = draw_text(draw, left, top, th, RED, "regular", 20)
-    left += th_size[0] + 6
-    draw_text(draw, left, 75, now.strftime("%B"), BLACK, "bold", 30)
-    # Draw on weather
-    draw_text(draw, 470, 20, "°C", BLACK, "regular", 28, align="left")
-    draw_text(draw, 470, 10, temp_current, BLACK, "regular", 90, align="right")
-    draw_text(draw, 550, 15, temp_max, RED, "regular", 40, align="centre")
-    draw_text(draw, 550, 60, temp_min, BLACK, "regular", 40, align="centre")
+    image = composer.render()
     # Send to client
     output = BytesIO()
     image.save(output, "PNG")
@@ -69,21 +40,247 @@ def index():
     return send_file(output, mimetype="image/png")
 
 
-def draw_text(draw, x, y, text, colour, font_style, font_size, align="left"):
-    """
-    Draws text and returns its size
-    """
-    # Get font
-    if (font_style, font_size) not in fonts:
-        fonts[font_style, font_size] = ImageFont.truetype(
-            "Roboto-%s.ttf" % font_style.title(), size=font_size
+class ImageComposer:
+    def __init__(self, api_key, lat, long, timezone):
+        self.api_key = api_key
+        self.lat = lat
+        self.long = long
+        self.timezone = pytz.timezone(timezone)
+
+    def render(self):
+        # Fetch weather
+        weather = WeatherClient(self.lat, self.long)
+        weather.load(self.api_key)
+        # Work out time
+        now = datetime.datetime.now(self.timezone)
+        # Create image
+        self.image = Image.new("P", IMAGE_SIZE, 0)
+        self.image.putpalette(PALETTE)
+        # Draw on date
+        self.draw = ImageDraw.ImageDraw(self.image)
+        left = 20
+        top = 10
+        self.draw_text(
+            pos=(left, top), text=now.strftime("%A"), colour=BLACK, font=("light", 60),
         )
-    # Calculate size
-    size = draw.textsize(str(text), font=fonts[font_style, font_size])
-    # Draw
-    if align == "right":
-        x -= size[0]
-    elif align.startswith("cent"):
-        x -= size[0] / 2
-    draw.text((x, y), str(text), fill=colour, font=fonts[font_style, font_size])
-    return size
+        top += 65
+        day_size = self.draw_text(
+            pos=(left, top),
+            text=now.strftime("%d").lstrip("0"),
+            colour=RED,
+            font=("regular", 30),
+        )
+        left += day_size[0] + 2
+        th = {
+            "01": "st",
+            "02": "nd",
+            "03": "rd",
+            "21": "st",
+            "22": "nd",
+            "23": "rd",
+            "31": "st",
+        }.get(now.strftime("%d"), "th")
+        th_size = self.draw_text(
+            pos=(left, top), text=th, colour=RED, font=("regular", 20)
+        )
+        left += th_size[0] + 6
+        self.draw_text(
+            pos=(left, 75), text=now.strftime("%B"), colour=BLACK, font=("bold", 30),
+        )
+        # Draw on weather header
+        self.draw_text(
+            pos=(470, 10),
+            text=round(weather.temp_current()),
+            colour=BLACK,
+            font=("regular", 90),
+            align="right",
+        )
+        self.draw_text(
+            pos=(470, 20), text="°C", colour=BLACK, font=("regular", 28), align="left",
+        )
+        temp_min, temp_max = weather.temp_range_24hr()
+        self.draw_text(
+            pos=(550, 15),
+            text=round(temp_max),
+            colour=RED,
+            font=("regular", 40),
+            align="centre",
+        )
+        self.draw_text(
+            pos=(550, 60),
+            text=round(temp_min),
+            colour=BLACK,
+            font=("regular", 40),
+            align="centre",
+        )
+        # Draw immediate weather
+        for (time_offset, left) in [(0, 30), (2600 * 2, 150), (3600 * 6, 270)]:
+            top = 130
+            summary = weather.hourly_summary(time_offset)
+            self.draw_weather_column(summary, top, left)
+        # Draw tomorrow
+        left = 450
+        summary = weather.daily_summary(1)
+        self.draw_weather_column(summary, top, left)
+        # Draw on data footer
+        left = 20
+        top = 390
+        self.draw_icon("sunrise", (left, top), (50, 50))
+        left += 50
+        self.draw_text(
+            pos=(left + 5, top + 10),
+            text=weather.sunrise().astimezone(self.timezone).strftime("%H:%M"),
+            colour=BLACK,
+            font=("bold", 30),
+        )
+        left = 180
+        self.draw_icon("sunset", (left, top), (50, 50))
+        left += 50
+        self.draw_text(
+            pos=(left + 5, top + 10),
+            text=weather.sunset().astimezone(self.timezone).strftime("%H:%M"),
+            colour=BLACK,
+            font=("bold", 30),
+        )
+        # Done!
+        return self.image
+
+    def draw_weather_column(self, summary, top, left):
+        # Weather icon
+        self.draw_icon(summary["icon"], (left, top + 40), (100, 100))
+        # Date/time heading
+        if "date" in summary:
+            time_text = summary["date"].astimezone(self.timezone).strftime("%A").title()
+        else:
+            time_text = (
+                summary["time"].astimezone(self.timezone).strftime("%-I%p").lower()
+            )
+        self.draw_text(
+            pos=(left + 50, top),
+            text=time_text,
+            colour=BLACK,
+            font=("bold", 25),
+            align="centre",
+        )
+        # Temperature
+        if "temperature_range" in summary:
+            self.draw_text(
+                pos=(left + 20, top + 150),
+                text=round(summary["temperature_range"][1]),
+                colour=RED,
+                font=("regular", 30),
+                align="centre",
+            )
+            self.draw_text(
+                pos=(left + 50, top + 150),
+                text="/",
+                colour=BLACK,
+                font=("regular", 30),
+                align="centre",
+            )
+            self.draw_text(
+                pos=(left + 80, top + 150),
+                text=round(summary["temperature_range"][0]),
+                colour=BLACK,
+                font=("regular", 30),
+                align="centre",
+            )
+        else:
+            temp_width = (
+                self.size_text(round(summary["temperature"]), ("regular", 30))[0]
+                + self.size_text("°C", ("regular", 20))[0]
+                + 3
+            )
+            self.draw_text(
+                pos=(left + 50 - (temp_width / 2), top + 150),
+                text=round(summary["temperature"]),
+                colour=RED,
+                font=("regular", 30),
+                align="left",
+            )
+            self.draw_text(
+                pos=(left + 50 + (temp_width / 2), top + 160),
+                text="°C",
+                colour=BLACK,
+                font=("regular", 20),
+                align="right",
+            )
+        # Wind
+        wind_width = (
+            self.size_text(round(summary["wind"]), ("regular", 30))[0]
+            + self.size_text("mph", ("regular", 20))[0]
+            + 3
+        )
+        self.draw_text(
+            pos=(left + 50 - (wind_width / 2), top + 190),
+            text=round(summary["wind"]),
+            colour=BLACK,
+            font=("regular", 30),
+            align="left",
+        )
+        self.draw_text(
+            pos=(left + 50 + (wind_width / 2), top + 200),
+            text="mph",
+            colour=BLACK,
+            font=("regular", 20),
+            align="right",
+        )
+
+    def draw_text(self, pos, text, colour, font, align="left"):
+        """
+        Draws text and returns its size
+        """
+        # Get font
+        if font not in fonts:
+            fonts[font] = ImageFont.truetype(
+                "Roboto-%s.ttf" % font[0].title(), size=font[1]
+            )
+        # Calculate size
+        size = self.draw.textsize(str(text), font=fonts[font])
+        # Draw
+        x, y = pos
+        if align == "right":
+            x -= size[0]
+        elif align.startswith("cent"):
+            x -= size[0] / 2
+        self.draw.text((x, y), str(text), fill=colour, font=fonts[font])
+        return size
+
+    def size_text(self, text, font):
+        """
+        Returns text size
+        """
+        # Get font
+        if font not in fonts:
+            fonts[font] = ImageFont.truetype(
+                "Roboto-%s.ttf" % font[0].title(), size=font[1]
+            )
+        # Calculate size
+        return self.draw.textsize(str(text), font=fonts[font])
+
+    def draw_icon(self, icon, pos, size):
+        """
+        Draws an icon file onto the image.
+        """
+        # Load icon
+        if icon not in icons:
+            raw_icon = Image.open(
+                os.path.join(os.path.dirname(__file__), "icons", icon + ".png")
+            ).convert("RGBA")
+            palette_icon = Image.new("P", raw_icon.size, 0)
+            for x in range(raw_icon.size[0]):
+                for y in range(raw_icon.size[1]):
+                    color = raw_icon.getpixel((x, y))
+                    new_color = BLACK
+                    if color[3] < 125:
+                        new_color = WHITE
+                    elif color[0] > 125:
+                        if color[1] < 125:
+                            new_color = RED
+                        else:
+                            new_color = WHITE
+                    palette_icon.putpixel((x, y), new_color)
+            icons[icon] = palette_icon
+        # Resize
+        icon_image = icons[icon].resize(size)
+        self.image.paste(icon_image, pos)
---
+++ server/weather.py
@@ -0,0 +1,95 @@
+import time
+import pytz
+import requests
+from datetime import datetime
+
+
+class WeatherClient:
+    def __init__(self, latitude, longitude):
+        self.latitude = float(latitude)
+        self.longitude = float(longitude)
+
+    def load(self, api_key):
+        self.data = requests.get(
+            f"https://api.openweathermap.org/data/2.5/onecall?lat={self.latitude}&lon={self.longitude}&exclude=minutely&units=metric&appid={api_key}"
+        ).json()
+        self.current_time = self.data["current"]["dt"]
+
+    def temp_current(self):
+        return self.data["current"]["temp"]
+
+    def temp_range_24hr(self):
+        temps = [
+            hour["temp"]
+            for hour in self.data["hourly"]
+            if hour["dt"] - self.current_time < 86400
+        ]
+        return min(temps), max(temps)
+
+    def humidity_current(self):
+        return self.data["current"]["humidity"]
+
+    def sunrise(self):
+        return datetime.utcfromtimestamp(self.data["current"]["sunrise"]).replace(
+            tzinfo=pytz.utc
+        )
+
+    def sunset(self):
+        return datetime.utcfromtimestamp(self.data["current"]["sunset"]).replace(
+            tzinfo=pytz.utc
+        )
+
+    def hourly_summary(self, time_offset):
+        # Find the right hour
+        target = time.time() + time_offset
+        for d1, d2 in zip(self.data["hourly"], self.data["hourly"][1:]):
+            if d1["dt"] < target < d2["dt"]:
+                break
+        data = d1
+        # Format a summary
+        return {
+            "time": datetime.utcfromtimestamp(data["dt"]).replace(tzinfo=pytz.utc),
+            "icon": self.code_to_icon(data["weather"][0]["id"]),
+            "description": data["weather"][0]["main"].title(),
+            "temperature": data["temp"],
+            "wind": data["wind_speed"] * 2.2,
+        }
+
+    def daily_summary(self, day_offset):
+        data = self.data["daily"][day_offset]
+        # Format a summary
+        return {
+            "date": datetime.utcfromtimestamp(data["dt"]).replace(tzinfo=pytz.utc),
+            "icon": self.code_to_icon(data["weather"][0]["id"]),
+            "description": data["weather"][0]["main"].title(),
+            "temperature_range": (data["temp"]["min"], data["temp"]["max"]),
+            "wind": data["wind_speed"] * 2.2,
+        }
+
+    def code_to_icon(self, code):
+        if code == 511:
+            return "snow"
+        elif code == 771:
+            return "thunderstorm"
+        elif 200 <= code < 300:
+            return "thunderstorm"
+        elif 300 <= code < 400:
+            return "showers"
+        elif 500 <= code < 505:
+            return "rain"
+        elif 520 <= code < 600:
+            return "showers"
+        elif 611 <= code < 620:
+            return "sleet"
+        elif 600 <= code < 700:
+            return "snow"
+        elif 700 <= code < 800:
+            return "fog"
+        elif code == 800:
+            return "clear-day"
+        elif code == 801:
+            return "clouds-few-day"
+        elif code == 802:
+            return "clouds-scattered"
+        else:
+            return "clouds-broken"
